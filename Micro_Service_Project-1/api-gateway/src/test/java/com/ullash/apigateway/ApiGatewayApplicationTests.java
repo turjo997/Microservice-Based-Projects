827427b23d1e6a5ed627a23c8bb34e468c6dcdef
turjo997 <turja997@gmail.com>
2023-08-08 18:23:01 +0600

    micro-service project - 1 files added

---
+++ Micro_Service_Project-1/api-gateway/src/test/java/com/ullash/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ullash.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
